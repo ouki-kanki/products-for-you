3e864ce5c1122544b9ae981d54722b2c56cda08c
killerwhale <alexandrisanas@gmail.com>
2023-06-14 23:38:12 +0300

    add support for sccs, css-modules

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Test from './components/Test';
+import { TestTwo } from './components/TestTwo';
+
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -31,6 +35,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <Test/>
+      <TestTwo/>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ client/src/components/TestTwo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import classes from './testTwo.module.scss'
+
+
+export const TestTwo = () => {
+  return (
+    <div className={classes.headerTop}>TestTwo</div>
+  )
+}
