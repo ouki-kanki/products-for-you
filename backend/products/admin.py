0a8c21f20bd9599b46f774a6c0059fc9b735834e
killerwhale <alexandrisanas@gmail.com>
2023-06-18 00:54:32 +0300

    changes in categories table, fix a conflict related to migrations & foreign key of categories

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Product, ProductItem, Category
 
 # Register your models here.
+admin.site.register((Product, ProductItem, Category),)
--- backend/products/models.py
+++ backend/products/models.py
@@ -1,26 +1,23 @@
 from django.db import models
 from user_control.models import CustomUser as User
 
-Categories = (
-    ("toys", "toys"),
-    ("phone cases", "phone cases"),
-    ("shoes", "shoes"),
-    ("hard drives", "hard drives"),
-    ("monitors", "monitors"),
-    ("gpu", "gpu"),
-    ("basketballs", "basketballs")
-)
-
-
 
 # TODO: should i put variations here or add a diff app?
-
+# NOTE: models.PROTECT it seems that does not allow null=True
 class Category(models.Model):
-    name = models.CharField(max_length=20, choices=Categories)
-    parent_category = models.ForeignKey("self", on_delete=models.SET_DEFAULT, default="category not defined")
+    name = models.CharField(max_length=255)
+    parent_category = models.ForeignKey("self", on_delete=models.SET_NULL, related_name='parent', blank=True, null=True)
     icon = models.CharField()
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+    # TODO: maybe this field is usefull
+    # is_parent = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
 
 
 
@@ -31,24 +28,29 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, default='')
-    # TODO: check if set null is the correct approach
-    category = models.ForeignKey(Category, on_delete=models.SET_DEFAULT, default="category not defined")
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='products')
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
-    # deleted_at = NOT IMPLEMENTED
+
+    class meta:
+        verbose_name = "Product"
 
 
-# TODO: how can i join with variations
+# TODO: how can i join with variations?
 class ProductItem(models.Model):
     '''
     product table - item will have different price in relation with the different variations
     '''
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_variation')
     sku = models.CharField(max_length=255)
     quantity = models.IntegerField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Product Variation"
+        db_table_comment = "Variation of Product"
 
 
 
