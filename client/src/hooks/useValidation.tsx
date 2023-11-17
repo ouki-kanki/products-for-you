89d20dacf0076e92e27939d8a93a27bced8cc34e
killerwhale <alexandrisanas@gmail.com>
2023-11-17 20:44:28 +0200

    add custom hook for validation
    - validates email & password, shows the strenght of the password

--- client/src/hooks/useValidation.tsx
+++ client/src/hooks/useValidation.tsx
@@ -7,7 +7,8 @@ interface IValidation {
     emailError?: string;
     passwordError?: string;
     isEmptyError?: string
-  }
+  },
+  passwordStrength: string
 }
 
 type TrackerField = RegExpMatchArray | null
@@ -20,15 +21,49 @@ interface IPasswordTracker {
   hasTwelveChars: TrackerField;
 }
 
+const passHash = {
+  1: 'tier 5',
+  2: 'tier 4', 
+  3: 'tier 3',
+  4: 'tier 2',
+  5: 'tier 1',
+  6: 'mirror tier'
+}
+
+
+/**
+ * @typedef {Object} ValidationObj
+ * @property {booelan} isValid
+ * @property {Object} errors
+ * @property {string} passwordStrength
+ */
+
+/**
+ * @typedef {Object} MainValObj
+ * @property {string} email
+ * @property {function} setEmail
+ * @property {string} password
+ * @property {function} setPassword
+ * @property {function} validateInputs
+ * @property {ValidationObj} validationObj
+ */
+
+/**
+ * @param 
+ * @returns {MainValObj} object with validation parameters
+ * @property {Object} validationObj -  
+ */ 
 export const useValidation = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [meter, setMeter] = useState(false)
-  const [validation, setValidation] = useState<IValidation>({
+  const [validationObj, setValidation] = useState<IValidation>({
     isValid: false,
-    errors: {}
+    errors: {},
+    passwordStrength: ''
   })
 
+
+  // ** email validation **
   const validateEmail = (value: string): string | undefined => {
     const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
 
@@ -36,34 +71,23 @@ export const useValidation = () => {
   }
 
   
-  const validatePassword = (value) => {
+  // ** main validation for pass **
+  const validatePassword = (value: string) => {
     const hasUppercase = /[A-Z]g/
     const hasNumeric = /[0-9]/g
     const hasASpecialCharacter = /[#?!@$%~^*-]/g
     const isAtLeastEightChars = /.{8,}/g
     const isAtLeastTwelveChars = /.{12,}/g
 
-
-
     const passwordTracker: IPasswordTracker = {
-      upperCase: password.match(hasUppercase),
-      number: password.match(hasNumeric),
-      specialChar: password.match(hasASpecialCharacter),
-      hasEightChars: password.match(isAtLeastEightChars),
-      hasTwelveChars: password.match(isAtLeastTwelveChars)
-    }
+        upperCase: value.match(hasUppercase),
+        number: value.match(hasNumeric),
+        specialChar: value.match(hasASpecialCharacter),
+        hasEightChars: value.match(isAtLeastEightChars),
+        hasTwelveChars: value.match(isAtLeastTwelveChars)
+      }
 
-    const passHash = {
-      1: 'tier 5',
-      2: 'tier 4', 
-      3: 'tier 3',
-      4: 'tier 2',
-      5: 'tier 1',
-      6: 'mirror tier'
-    }
-
-
-    const passwordStrength = (tracker: IPasswordTracker) => {
+    const getPasswordStrength = (tracker: IPasswordTracker) => {
       // TODO finde a more elegant way 
       const trackerlength: Exclude<Ran<6>, 0> = Object.keys(tracker).length as Exclude<Ran<6>, 0>
       
@@ -78,12 +102,54 @@ export const useValidation = () => {
       }
     }
 
-  } 
+    const validatePass = (tracker: IPasswordTracker) => {
+      if (!tracker.hasEightChars) {
+        return 'length has to be at least 8 characters'
+      }
 
+      if (!tracker.number) {
+        return 'password has to have at least one number'
+      }
 
+      if (!tracker.specialChar) {
+        return 'password has to have at least one special character'
+      }
 
+      if (!tracker.upperCase) {
+        return 'passs has to have at least one uppercase letter'
+      } 
+      return undefined
+    }  
+    return {
+      passwordStrength: getPasswordStrength(passwordTracker),
+      passwordError: validatePass(passwordTracker)
+    }
+  }
+
+  // ** main Validation **
+  const validateInputs = () => {
+    const { passwordStrength, passwordError } = validatePassword(password)
+    const emailError = validateEmail(email)
+
+    const isValid = !passwordError && !emailError
+
+    setValidation({
+      isValid,
+      errors: {
+        passwordError,
+        emailError
+      },
+      passwordStrength
+    })
+
+  }
 
   return {
-
+    email,
+    setEmail,
+    password,
+    setPassword,
+    validateInputs,
+    validationObj
   }
 }
